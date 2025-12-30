c403bafa93d15acd4c7dbecabe7ab2d3ce014694
kavi <sonikavani5@gmail.com>
2025-12-30 01:33:44 +0100

    function create team update

--- src/chapters/chapter_4.py
+++ src/chapters/chapter_4.py
@@ -1,9 +1,9 @@
 #Guided scenario: Quidditch match
-
-
+import random
 
 from src.universe.character import Character
 from src.universe.house import House
+from src.utils.input_utils import affichage_lettre_par_lettre
 
 
 def create_team(house_name, team_data, is_player=False, player=None):
@@ -32,3 +32,23 @@ def create_team(house_name, team_data, is_player=False, player=None):
     }
 
     return team
+
+def attempt_goal(attacking_team, defending_team, player_is_seeker=False):
+    chance_goal= random.randint(1,10)
+    players_list = attacking_team["players"]
+
+    if chance_goal >= 6:
+        if player_is_seeker==True:
+            scorer= players_list[0]
+        else:
+            which_player= random.randint(1,len(players_list)-1)
+            scorer= players_list[which_player]
+
+        attacking_team["score"] += 10
+        attacking_team["goals_scored"] += 1
+        affichage_lettre_par_lettre("{} scores a goal for {}! (+10 points)".format(scorer, attacking_team["name"]))
+    else:
+        defending_team["goals_blocked"] += 1
+        affichage_lettre_par_lettre("{} blocks the attack!)".format(defending_team["name"])
+
+
