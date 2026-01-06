05a8977aad84999bb2ce424566a4851439fca278
Mathieu BOGET <mathieu.boget@gmail.com>
2026-01-06 14:44:39 +0100

    Refactor la fonction create_team pour simplifier la création de l'équipe et améliorer la lisibilité du code

--- src/chapters/chapter_4.py
+++ src/chapters/chapter_4.py
@@ -18,20 +18,13 @@ import os
 
 
 def create_team(house_name, team_data, is_player=False, player=None):
-    """
-    Creates a team dictionary from the JSON data.
-    If it is the player's team, replaces the Seeker with the player's name.
-    """
-    # Create a copy of the players list to avoid modifying the original loaded data
+
     players_list = team_data["players"].copy()
 
-    # If this is the player's team, replace the default Seeker (Index 0)
     if is_player and player:
-        # Construct player's display name
         player_display_name = f"{player.first_name} {player.last_name} (Seeker)"
         players_list[0] = player_display_name
 
-    # Initialize the team dictionary structure
     team = {
         "name": house_name,
         "score": 0,
